c7800cdbabd7cd1424318da40dc42ccb2363a52f
James Woodley <wooderz@Jamess-MacBook-Pro.local>
2018-08-17 11:51:16 +0100

    Fire the RefreshTasks websocket call when Reenqueuing Tasks.

--- Microphobia.Dashboard/Controllers/TasksController.cs
+++ Microphobia.Dashboard/Controllers/TasksController.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using N17Solutions.Microphobia.Dashboard.Models;
 using N17Solutions.Microphobia.ServiceContract.Providers;
+using N17Solutions.Microphobia.ServiceContract.Websockets.Hubs;
 
 namespace N17Solutions.Microphobia.Dashboard.Controllers
 {
@@ -11,10 +12,12 @@ namespace N17Solutions.Microphobia.Dashboard.Controllers
     public class TasksController : DashboardController
     {
         private readonly IDataProvider _dataProvider;
+        private readonly MicrophobiaHubContext _hubContext;
         
-        public TasksController(IDataProvider dataProvider)
+        public TasksController(IDataProvider dataProvider, MicrophobiaHubContext hubContext)
         {
             _dataProvider = dataProvider;
+            _hubContext = hubContext;
         }
         
         [HttpGet]
@@ -39,6 +42,8 @@ namespace N17Solutions.Microphobia.Dashboard.Controllers
             var task = await _dataProvider.GetTaskInfo(taskId);
             task.Status = TaskStatus.Created;
             await _dataProvider.Save(task);
+
+            await _hubContext.RefreshTasks();
 
             return Ok();
         }
