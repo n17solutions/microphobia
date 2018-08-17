249687779d165aa5745aa0b0678deff68d1aa8b6
James Woodley <info@n17solutions.com>
2018-08-17 13:31:46 +0100

    Add Migration to drop IsAsync column.

---
+++ Microphobia.Postgres.Migrations.Task/Migrations/20180817123114_RemoveIsAsyncColumn.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace N17Solutions.Microphobia.Postgres.Migrations.Task.Migrations
+{
+    public partial class RemoveIsAsyncColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAsync",
+                schema: "microphobia",
+                table: "TaskInfo");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAsync",
+                schema: "microphobia",
+                table: "TaskInfo",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
